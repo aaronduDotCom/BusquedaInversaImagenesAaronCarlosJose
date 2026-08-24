1c1e26cad193a5ed8ef7c5a9a5e63b7933542a4d
MIguel2043 <josemiguelrm3@gmail.com>
2026-08-24 15:12:28 -0600

    creación de la clase nodo y la los metodos de la coleccion, hice todos los obligatorios

--- src/Model/NodoImagen.java
+++ src/Model/NodoImagen.java
@@ -2,5 +2,34 @@ package Model;
 // nodo de la lista doble
 // doble puntero
 
-public class NodoImagen {
+public class NodoImagen<T> { // la T es como vimos en clases para que pueda ser generico el tipo de dato
+    private T valor;
+    private NodoImagen<T> anterior;
+    private NodoImagen<T> siguiente;
+
+    public NodoImagen(T valor){ this.valor = valor; }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+    public T getValor() {
+        return valor;
+    }
+
+    public NodoImagen<T> getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoImagen<T> anterior) {
+        this.anterior = anterior;
+    }
+
+    public NodoImagen<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoImagen<T> siguiente) {
+        this.siguiente = siguiente;
+    }
 }
