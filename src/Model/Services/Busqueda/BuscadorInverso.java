78c62c9953491ca2547950559b4f57f172d00ef6
aa <aaron.delgado.urena@est.una.ac.cr>
2026-09-07 11:56:04 -0600

    Metodos similitud terminados, pro favor revisar

--- src/Model/Services/Busqueda/BuscadorInverso.java
+++ src/Model/Services/Busqueda/BuscadorInverso.java
@@ -1,7 +1,5 @@
 package Model.Services.Busqueda;
 
-import Model.Estructuras.ColeccionImagenData;
-import Model.Imagen.Imagen;
 import Model.Imagen.ImagenData;
 
 //recibe camina por la coleccion de imagenes data y va buscando
--- src/Model/Services/Similitud/SimilitudCoseno.java
+++ src/Model/Services/Similitud/SimilitudCoseno.java
@@ -1,13 +1,11 @@
 package Model.Services.Similitud;
 
-import Model.Estructuras.ColeccionImagen;
 import Model.Estructuras.ColeccionImagenData;
 import Model.Estructuras.ColeccionResultadoImagenData;
-import Model.Estructuras.Vector;
 import Model.Imagen.ImagenData;
 import Model.Imagen.ResultadoImagenData;
 
-public class SimilitudCoseno { //Entre mas pequeño mas cerca
+public class SimilitudCoseno { //Entre mas cerca de 1 mejor, -1 < x < 1
     public double simiCos(ImagenData a, ImagenData b){
         //     A * B
         //   ---------
@@ -50,9 +48,7 @@ public class SimilitudCoseno { //Entre mas pequeño mas cerca
 
             cID.getIterador().next();
         }
-
         cRID.ordenarMergeSort();
-
         return cRID;
     }
 }
